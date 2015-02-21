c9f7901c70a96cb93576d1c156711b0aedebf4d4
JaskirtPooni <jaskirt.pooni@gmail.com>
2015-02-21 05:52:43 -0500

    Changed code to use servo library instead of PWM

--- controller-source/Test/quadTest/quadTest.ino
+++ controller-source/Test/quadTest/quadTest.ino
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <Servo.h>
 
 #define keyA 97
 #define keyS 115
@@ -6,26 +7,67 @@
 #define keyW 119
 #define keySpace 32
 #define keyC 99
+#define keyN 110
+#define keyM 109
 #define keyP 112
-#define sensitivity 1
+#define sensitivity 5
 #define ailPin 3
 #define elePin 5
 #define thrustPin 6
+#define rudderPin 9
 
 int serialData = 0;
+Servo pitchPPM;
+Servo rollPPM;
+Servo yawPPM;
+Servo thrustPPM;
 uint8_t pitch;
 uint8_t roll;
 uint8_t elevation;
+uint8_t yaw;
 bool changed = false;
 
+//void setPwmFrequency(int pin, int divisor) {
+//  byte mode;
+//  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+//    switch(divisor) {
+//      case 1: mode = 0x01; break;
+//      case 8: mode = 0x02; break;
+//      case 64: mode = 0x03; break;
+//      case 256: mode = 0x04; break;
+//      case 1024: mode = 0x05; break;
+//      default: return;
+//    }
+//    if(pin == 5 || pin == 6) {
+//      TCCR0B = TCCR0B & 0b11111000 | mode;
+//    } else {
+//      TCCR1B = TCCR1B & 0b11111000 | mode;
+//    }
+//  } else if(pin == 3 || pin == 11) {
+//    switch(divisor) {
+//      case 1: mode = 0x01; break;
+//      case 8: mode = 0x02; break;
+//      case 32: mode = 0x03; break;
+//      case 64: mode = 0x04; break;
+//      case 128: mode = 0x05; break;
+//      case 256: mode = 0x06; break;
+//      case 1024: mode = 0x7; break;
+//      default: return;
+//    }
+//    TCCR2B = TCCR2B & 0b11111000 | mode;
+//  }
+//}
 
 void setup() {
   pitch = 128;
   roll = 128;
-  elevation = 0;
-  pinMode(ailPin,OUTPUT);
-  pinMode(elePin,OUTPUT);
-  pinMode(thrustPin,OUTPUT);
+  elevation = 255;
+  yaw = 128;
+  rollPPM.attach(ailPin);
+  pitchPPM.attach(elePin);
+  thrustPPM.attach(thrustPin);
+  yawPPM.attach(rudderPin);
+//setPwmFrequency(thrustPin,128);
   Serial.begin(9600);
 }
 
@@ -34,51 +76,70 @@ void loop() {
   {
     serialData = Serial.read();
     if (serialData == keyA){
-      if (roll>0){
+      if (roll-sensitivity>0){
         roll -= sensitivity;
         changed = true;
       }
     }
     if (serialData == keyD){
-      if (roll<255){
+      if (roll+sensitivity<=255){
         roll += sensitivity;
         changed = true;
       }
     }
     if (serialData == keyS){
-      if (pitch>0){
+      if (pitch-sensitivity>=0){
         pitch -= sensitivity;
         changed = true;
       }
     }
     if (serialData == keyW){
-      if (pitch<255){
+      if (pitch+sensitivity<=255){
         pitch += sensitivity;
         changed = true;
       }
     }
     if (serialData == keyC){
-      if (elevation>0){
+      if (elevation-sensitivity>=0){
         elevation -= sensitivity;
         changed = true;
       }
     }
     if (serialData == keySpace){
-      if (elevation<255){
+      if (elevation+sensitivity<=255){
         elevation += sensitivity;
         changed = true;
       }
     }
+    if (serialData == keyN){
+      if (yaw-sensitivity>=0){
+        yaw -= sensitivity;
+        changed = true;
+      }
+    }
+    if (serialData == keyM){
+      if (yaw+sensitivity<=255){
+        yaw += sensitivity;
+        changed = true;
+      }
+    }
     if (serialData == keyP){
+      Serial.print("Pitch: \t");
       Serial.println(pitch);
+      Serial.print("Roll: \t");
       Serial.println(roll);
+      Serial.print("Thrust: ");
       Serial.println(elevation);
+      Serial.print("Yaw: \t");
+      Serial.println(yaw);
     }
   }
   if (changed == true){
-    analogWrite(ailPin, roll);
-    analogWrite(elePin, pitch);
-    analogWrite(thrustPin, elevation);
+    
+    rollPPM.write(map(roll, 0, 255, 0, 179));
+    pitchPPM.write(map(pitch, 0, 255, 0, 179));
+    thrustPPM.write(map(elevation, 0, 255, 0, 179));
+    yawPPM.write(map(yaw, 0, 255, 0, 179));
     changed = false;
   }
 }
