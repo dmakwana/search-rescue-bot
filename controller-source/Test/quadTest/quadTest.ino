e213fb268c5bb04cb6ba8e13578a7508b8cf6057
Digvijay Makwana <dmakwana@github.com>
2015-02-20 23:05:03 -0500

    modified controller source

--- controller-source/Test/quadTest/quadTest.ino
+++ controller-source/Test/quadTest/quadTest.ino
@@ -8,20 +8,24 @@
 #define keyC 99
 #define keyP 112
 #define sensitivity 1
-#define rollPin 3
-#define thrustPin 5
-#define elevationPin 6
+#define ailPin 3
+#define elePin 5
+#define thrustPin 6
 
 int serialData = 0;
 uint8_t pitch;
 uint8_t roll;
 uint8_t elevation;
+bool changed = false;
 
 
 void setup() {
   pitch = 128;
   roll = 128;
   elevation = 0;
+  pinMode(ailPin,OUTPUT);
+  pinMode(elePin,OUTPUT);
+  pinMode(thrustPin,OUTPUT);
   Serial.begin(9600);
 }
 
@@ -32,31 +36,37 @@ void loop() {
     if (serialData == keyA){
       if (roll>0){
         roll -= sensitivity;
+        changed = true;
       }
     }
     if (serialData == keyD){
       if (roll<255){
         roll += sensitivity;
+        changed = true;
       }
     }
     if (serialData == keyS){
       if (pitch>0){
         pitch -= sensitivity;
+        changed = true;
       }
     }
     if (serialData == keyW){
       if (pitch<255){
         pitch += sensitivity;
+        changed = true;
       }
     }
     if (serialData == keyC){
       if (elevation>0){
         elevation -= sensitivity;
+        changed = true;
       }
     }
     if (serialData == keySpace){
       if (elevation<255){
         elevation += sensitivity;
+        changed = true;
       }
     }
     if (serialData == keyP){
@@ -65,7 +75,10 @@ void loop() {
       Serial.println(elevation);
     }
   }
-  analogWrite(rollPin, roll);
-  analogWrite(pitch, pitch);
-  analogWrite(elevationPin, elevation);
+  if (changed == true){
+    analogWrite(ailPin, roll);
+    analogWrite(elePin, pitch);
+    analogWrite(thrustPin, elevation);
+    changed = false;
+  }
 }
