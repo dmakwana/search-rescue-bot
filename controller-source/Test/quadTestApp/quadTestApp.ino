f5a3c711d0c74ca2602e3adb5e85c3de9f47ac2c
JaskirtPooni <jaskirt.pooni@gmail.com>
2015-03-11 03:51:29 -0400

    Added Hold

--- controller-source/Test/quadTestApp/quadTestApp.ino
+++ controller-source/Test/quadTestApp/quadTestApp.ino
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <Servo.h> 
+#include <NewPing.h>
 
 int rollNeutral = 1500;
 #define throttleMax 2000
@@ -18,11 +19,11 @@ int pitchNeutral = 1500;
 
 #define rollPin 3
 #define pitchPin 5
-#define throttlePin 6
+#define throttlePin 11
 #define yawPin 9
 #define flightPin 10
-#define SerialTx 2
-#define SerialRx 4
+#define echoPin 51
+#define trigPin 53
 #define battery 1
 
 #define sensitivity 25
@@ -44,6 +45,8 @@ int pitchNeutral = 1500;
 #define bluetooth Serial1
 #define negTr -2
 #define posTr 2
+#define k 3 
+
 
 int serialData = 0;
 Servo pitchPWM;
@@ -62,6 +65,13 @@ int cmdIndex;
 String st;
 int tr;
 char c;
+int change=1;
+long duration;
+long distance;
+bool hold = false;
+long holdVal=0;
+long throttleVal=0;
+int error=0;
 
 int getVal() {
   
@@ -84,6 +94,13 @@ void setNeutral(){
   flight = flightMode1;
 }
 
+NewPing sonar(trigPin, echoPin, 200);
+
+int getDistance(){
+  return sonar.ping_cm();
+}
+  
+
 //void SerialConfig(){
 //}
 
@@ -96,7 +113,9 @@ void setup() {
   throttlePWM.attach(throttlePin);
   yawPWM.attach(yawPin);
   flightPWM.attach(flightPin);
-  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  //throttlePWM.writeMicroseconds(1000);
   delay(1000);  // Short delay, wait for the Mate to send back CMD
   Serial.begin(115200);
   bluetooth.begin(115200);
@@ -104,10 +123,12 @@ void setup() {
 }
 
 void loop() {
+  getDistance();
   if (bluetooth.available() > 0)
   {
     c = (char)bluetooth.read();
     if(c=='\n') {
+      hold=false;
       cmd[cmdIndex] = 0;
       
       if (cmd[0] == (char)unarm){
@@ -129,9 +150,14 @@ void loop() {
       }
       else if (cmd[0] == (char)keyS){
         throttle = getVal();
-        Serial.print("Throttle:");
-        Serial.println(throttle);
+        //Serial.print("Throttle:");
+        //Serial.println(throttle);
         changed = true;
+      }
+      else if (cmd[0] == 'h'){
+        hold=true;
+        holdVal=getDistance();
+        throttleVal=throttle;
       }
       else if (cmd[0] == 'p'){
         if (cmd[1] == '+'){
@@ -144,6 +170,32 @@ void loop() {
         st = (String)"pt " + pitchNeutral + "\n";
         bluetooth.print(st);
         Serial.print("pitch trim: ");
+        Serial.println(pitchNeutral);  
+      }
+      else if (cmd[0] == 'f'){
+        changed=true;
+        Serial.print("hi");
+        if (cmd[1] == '+'){
+          pitch+=change;
+        }
+        else if (cmd[1] == '-'){
+          pitch-=change;
+        }
+        st = (String)"fl " + pitch + "\n";
+        bluetooth.print(st);
+        Serial.print("pitch level: ");
+        Serial.println(pitch);  
+      }
+      else if (cmd[0] == 'c'){
+        if (cmd[1] == '+'){
+          change++;
+        }
+        else if (cmd[1] == '-'){
+          change--;
+        }
+        st = (String)"cl " + change + "\n";
+        bluetooth.print(st);
+        Serial.print("change amount: ");
         Serial.println(pitchNeutral);  
       }
       else if (cmd[0] == 't'){
@@ -185,16 +237,27 @@ void loop() {
     } else {      
       cmd[cmdIndex] = c;
       if(cmdIndex<99) cmdIndex++;
-    }
-   
-    if (changed == true){
+    }    
+  }
+  if (changed == true){
+      //Serial.println(throttle);
       rollPWM.writeMicroseconds(roll);
       pitchPWM.writeMicroseconds(pitch);
       throttlePWM.writeMicroseconds(throttle);
       yawPWM.writeMicroseconds(yaw);
       flightPWM.writeMicroseconds(flight);
       changed = false;
+  }
+  if (hold){
+    error=holdVal-getDistance();
+    Serial.print("Error: ");
+    Serial.println(error);
+    if (throttleVal+error*k<2000 && throttleVal+error*k>1000){
+      throttle=throttleVal+error*k;
     }
+    Serial.print("Throttle: ");
+    Serial.println(throttle);
+    changed=true;
   }
 }
 
